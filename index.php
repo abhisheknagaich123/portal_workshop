c7c2099a7fdc926d5d0e62daf6802f78e0309a2c
Abhishek Nagaich <abhishek.nagaich_csd18@gla.ac.in>
2022-11-19 22:49:17 +0530

    commit

--- index.php
+++ index.php
@@ -10,65 +10,45 @@ while($row=mysqli_fetch_assoc($cat_res)){
 }
 ?>
 
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
   <head>
-    <title>Workshop &mdash; abhishek@</title>
+    <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <link href="https://fonts.googleapis.com/css?family=Roboto+Mono" rel="stylesheet">
-    <link rel="stylesheet" href="fonts/icomoon/style.css">
-    <link rel="stylesheet" href="css/bootstrap.min.css">
-    <link rel="stylesheet" href="css/magnific-popup.css">
-    <link rel="stylesheet" href="css/jquery-ui.css">
-    <link rel="stylesheet" href="css/owl.carousel.min.css">
-    <link rel="stylesheet" href="css/owl.theme.default.min.css">
-    <link rel="stylesheet" href="css/bootstrap-datepicker.css">
-    <link rel="stylesheet" href="fonts/flaticon/font/flaticon.css">
-    <link rel="stylesheet" href="css/aos.css">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+     
+    <script src="https://cdn.tailwindcss.com"></script>
+    <!-- Font Awesome -->
+<link
+  href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css"
+  rel="stylesheet"
+/>
+<!-- Google Fonts -->
+<link
+  href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+  rel="stylesheet"
+/>
+<!-- MDB -->
+<link
+  href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/6.0.0/mdb.min.css"
+  rel="stylesheet"
+/>
 
-    <link rel="stylesheet" href="css/style.css">
+    <title>Hello, world!</title>
   </head>
-  <body>
-
- <header class="site-navbar py-3" role="banner">
-      <div class="container-fluid">
-        <div class="row align-items-center">
-          <div class="col-11 col-xl-2">
-            <h1 class="mb-0"><a href="index.html" class="text-white h2 mb-0">Work<span class="text-primary">shop</span> </a></h1>
-          </div>
-          <div class="col-12 col-md-10 d-none d-xl-block">
-            <nav class="site-navigation position-relative text-right" role="navigation">
-              <ul class="site-menu js-clone-nav mx-auto d-none d-lg-block">
-                <li class="active"><a href="index.php">Home</a></li>
-                <li class="active"><a href="#">about</a></li>
-                <?php
-                    foreach($cat_arr as $list){
-                      ?>
-                      <li><a href="buy-tickets.php?id=<?php echo $list['id']?>"><?php echo $list['categories']?></a></li>
-
-                      <?php
-                    }
-                    ?>
-                <li><a href="contact.php">Contacts</a></li>
-                <li class="cta"><a href="buy-tickets.php">Buy Tickets</a></li>
-              </ul>
-            </nav>
-          </div>
-          <div class="d-inline-block d-xl-none ml-md-0 mr-auto py-3" style="position: relative; top: 3px;"><a href="#" class="site-menu-toggle js-menu-toggle text-white"><span class="icon-menu h3"></span></a></div>
-          </div>
-        </div>
-      </div>
-    </header>
- 
+  <body  style="background-color:  #FFB6C1">
+<?php 
+require('header.php');
+ ?> 
     
-   <?php
+
+  <?php
 $get_product=get_product($con,'','');
   ?>
-    
-
-<div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel">
+<div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel" >
   <ol class="carousel-indicators">
 
     <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
@@ -77,21 +57,21 @@ $get_product=get_product($con,'','');
   </ol>
   <div class="carousel-inner">
     <div class="carousel-item active">
-      <a href="#"><img src="<?php echo PRODUCT_IMAGE_SITE_PATH.$get_product['0']['image']?>" class="d-block w-100" style="width: 70%; height: 600px;" alt="..."></a>
+      <a href="#"><img src="<?php echo PRODUCT_IMAGE_SITE_PATH.$get_product['0']['image']?>" class="d-block w-100" style="width: 70%; height: 500px;" alt="..."></a>
       <div class="carousel-caption d-none d-md-block">
         <h5><?php echo $get_product['0']['workshop_name']?></h5>
 
       </div>
     </div>
     <div class="carousel-item">
-<a href="#"><img src="<?php echo PRODUCT_IMAGE_SITE_PATH.$get_product['1']['image']?>" class="d-block w-100" style="width: 70%; height: 600px;" alt="..."></a>
+<a href="#"><img src="<?php echo PRODUCT_IMAGE_SITE_PATH.$get_product['1']['image']?>" class="d-block w-100" style="width: 70%; height: 500px;" alt="..."></a>
       <div class="carousel-caption d-none d-md-block">
         <h5><?php echo $get_product['1']['workshop_name']?></h5>
 
       </div>
     </div>
     <div class="carousel-item">
-<a href="#"><img src="<?php echo PRODUCT_IMAGE_SITE_PATH.$get_product['2']['image']?>" class="d-block w-100" style="width: 70%; height: 600px;" alt="..."></a>
+<a href="#"><img src="<?php echo PRODUCT_IMAGE_SITE_PATH.$get_product['2']['image']?>" class="d-block w-100" style="width: 70%; height: 500px;" alt="..."></a>
       <div class="carousel-caption d-none d-md-block">
         <h5><?php echo $get_product['2']['workshop_name']?></h5>
         
@@ -108,106 +88,44 @@ $get_product=get_product($con,'','');
   </button>
 </div>
 
-
-      <div class="site-section">
-      <div class="container">
-        
-                           <?php
+                            <?php
                             $get_product=get_product($con,5);
                             foreach($get_product as $list){
                             ?>
-         <div class="row align-items-center speaker">
-          
-          <div class="col-lg-5 mb-5 mb-lg-0" data-aos="fade" data-aos-delay="100">
-            <img src="<?php echo PRODUCT_IMAGE_SITE_PATH.$list['image']?>" alt="Image" class="img-fluid">
-          </div>
-          <div class="bio pl-lg-5">
-                       <div class="bio pl-lg-5">
-              <span class="text-uppercase text-primary d-block mb-3" data-aos="fade-right" data-aos-delay="300"><?php echo $list['workshop_name']?></span>
-              <span class="text-uppercase text-primary d-block mb-3" data-aos="fade-right" data-aos-delay="300"><?php echo $list['categories']?></span>
-              <p class="mb-4" data-aos="fade-right" data-aos-delay="400"><?php echo $list['description']?></p>
-              <p data-aos="fade-right" data-aos-delay="500">
-    
-                <a href="#" class="p-2"><span class="icon-facebook"></span></a>
-                <a href="#" class="p-2"><span class="icon-twitter"></span></a>
-                <a href="#" class="p-2"><span class="icon-github"></span></a>
-              </p>
-              <a href="buy-tickets.php?id=<?php echo $list['id']?>" class="btn-custom" data-aos="fade-up" data-aos-delay="400"><span>Buy Tickets</span></a>
-              <div class="row" data-aos="fade-up" data-aos-delay="500">
-          <div class="col-12 text-center" style="padding-top: 25px">
-            <a href="about.php?id=<?php echo $list['id']?>" class="btn-custom" data-aos="fade-up" data-aos-delay="400"><span>More About us</span></a>
-          </div>
-        </div>
-            </div>
 
-          </div>
-        </div>
-        <?php } ?>
-
-
-        <div class="row" data-aos="fade-up" data-aos-delay="500">
-          <div class="col-12 text-center">
-            <a href="#" class="btn-custom" data-aos="fade-up" data-aos-delay="400"><span>go to 1st workshop</span></a>
-          </div>
-        </div>
+ <div>
+   <section class="text-gray-600 body-font">
+  <div class="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
+                          
+    <div class="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
+      <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900"><?php echo $list['workshop_name']?>
+        <br class="hidden lg:inline-block">
+      </h1>
+      <p class="mb-8 leading-relaxed"><?php echo $list['description']?></p>
+      <div class="flex justify-center">
+        
+      <a href="form.php?id=<?php echo $list['id']?>"><button class="inline-flex text-white bg-green-500 border-0 py-2 px-6 focus:outline-none hover:bg-green-600 rounded text-center">Registar Now</button> 
+</a>        
       </div>
     </div>
-      
-    <footer class="site-footer">
-      <div class="container">
-        <div class="row mb-5">
-          <div class="col-md-4">
-            <h2 class="footer-heading text-uppercase mb-4">About Event</h2>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit aliquid quibusdam fugit architecto.</p>
-          </div>
-          <div class="col-md-3 ml-auto">
-            <h2 class="footer-heading text-uppercase mb-4">Quick Links</h2>
-            <ul class="list-unstyled">
-              <li><a href="#">About Us</a></li>
-              
-              <li><a href="#">Contact Us</a></li>
-            </ul>
-          </div>
-          <div class="col-md-4">
-            <h2 class="footer-heading text-uppercase mb-4">Connect with Us</h2>
-            <p>
-              <a href="#" class="p-2 pl-0"><span class="icon-facebook"></span></a>
-              <a href="#" class="p-2"><span class="icon-twitter"></span></a>
-              <a href="#" class="p-2"><span class="icon-youtube"></span></a>
-              <a href="#" class="p-2"><span class="icon-instagram"></span></a>
-            </p>
-          </div>
-        </div>
-        <div class="row">
-          
-            <div class="col-md-12 text-center">
-              <div class="border-top pt-5">
-              <p>
-              <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-              Copyright &copy; <script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="icon-heart text-primary" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank" >abhishek</a>
-              <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-              </p>
-            </div>
-          </div>
-        </div>
-      </div>
-    </footer>
-    
+    <div class="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
+      <img class="object-cover object-center rounded" alt="hero" src="<?php echo PRODUCT_IMAGE_SITE_PATH.$list['image']?>">
+    </div>
   </div>
+</section>
+<?php } ?>
+ </div>
 
-  <script src="js/jquery-3.3.1.min.js"></script>
-  <script src="js/jquery-migrate-3.0.1.min.js"></script>
-  <script src="js/jquery-ui.js"></script>
-  <script src="js/popper.min.js"></script>
-  <script src="js/bootstrap.min.js"></script>
-  <script src="js/owl.carousel.min.js"></script>
-  <script src="js/jquery.stellar.min.js"></script>
-  <script src="js/jquery.countdown.min.js"></script>
-  <script src="js/jquery.magnific-popup.min.js"></script>
-  <script src="js/bootstrap-datepicker.min.js"></script>
-  <script src="js/aos.js"></script>
+<?php 
+require('footer.php');
+ ?> 
 
-  <script src="js/main.js"></script>
+
+    <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous"></script>
+
+    
     
 
 
